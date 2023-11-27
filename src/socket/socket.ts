f23ec10f18b7ecbac033fc6864520a731b3f69eb
Niladitya Sen <niladityasen.2212@gmail.com>
2023-11-27 11:55:56 +0530

    Update socket URL in socket.ts

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3015", {
+const socket = io("http://iqcanada.waysdatalabs.com/", {
     autoConnect: true
 });
 
 export default socket;
+
+// http://localhost:3015/
