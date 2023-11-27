b5dba9eb1c86f92e6958bbe0a1556ca1829f5bb9
Niladitya Sen <niladityasen.2212@gmail.com>
2023-11-27 12:32:59 +0530

    Update socket URL to use HTTPS

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://iqcanada.waysdatalabs.com/", {
+const socket = io("https://iqcanada.waysdatalabs.com/", {
     autoConnect: true
 });
 
